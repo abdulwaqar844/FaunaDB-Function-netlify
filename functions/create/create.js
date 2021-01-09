7c272477f5b0b459ca02cf9e705422aec2d794d5
Abdul Waqar <abdulwaqar844@gmaill.com>
2021-01-09 15:34:23 +0500

    Create operation updated

---
+++ functions/create/create.js
@@ -0,0 +1,27 @@
+const process = require('process')
+var faunadb = require('faunadb'),
+  q = faunadb.query;
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+  try {
+    const messageBody = JSON.parse(event.body);
+    var adminClient = new faunadb.Client({ secret: process.env.FAUNADB_SERVER_SECRET});
+    const result = await adminClient.query(
+      q.Create(
+        q.Collection('classes'),
+        { data: { name:  messageBody.name,fname:messageBody.fname}}
+      )
+    )
+    return {
+      statusCode: 200,
+        body: JSON.stringify({ name:  result.ref.id}),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+  }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,69 @@
-import React from "react"
-
+import React from 'react';
+import { Formik } from 'formik';
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <h1>FaunaDB CRUD</h1>
+    <Formik
+      initialValues={{ name: '' ,  fname: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.name) {
+          errors.name = 'Required';
+        } if (!values.fname) {
+          errors.fname = 'Required';
+        }
+        return errors;
+
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log(values);
+        fetch(`/.netlify/functions/create`, {
+          method: 'post',
+          body: JSON.stringify(values)
+        })
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+
+          });
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <input
+          placeholder="Enter Name"
+            type="text"
+            name="name"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.name}
+          />
+          {errors.name && touched.name && errors.name}
+<br/>
+          <input
+                    placeholder="Enter Father Name"
+            type="text"
+            name="fname"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.fname}
+          />
+          {errors.name && touched.name && errors.name}
+          <br/>
+          <button type="submit" disabled={isSubmitting}>
+            Add Message
+           </button>
+        </form>
+      )}
+    </Formik>
+  </div>
 }
